7949b07d504cfb830f169e41f4b6b8b519c6cb05
Pavithra-tkm <pavithratkm28@gmail.com>
2024-10-12 12:10:03 +0530

    Create create even no.c

---
+++ create even no.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int start, end, i;
+
+  
+    printf("Enter the starting number: ");
+    scanf("%d", &start);
+    
+    printf("Enter the ending number: ");
+    scanf("%d", &end);
+
+    printf("Even numbers between %d and %d are:\n", start, end);
+    
+    
+    for (i = start; i <= end; i++) {
+        if (i % 2 == 0) {
+            printf("%d ", i);
+        }
+    }
+    
+    printf("\n");
+    return 0;
+}
